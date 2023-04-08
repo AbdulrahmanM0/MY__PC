f2181ed7ac457e30d3874750874d3cf9f40e7daa
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2023-04-08 01:07:11 +0100

    Update main.js

--- main/main.js
+++ main/main.js
@@ -356,8 +356,10 @@ let music = document.getElementById('music');
 let deskTop = document.getElementsByClassName('desktop')[0];
 let sendMSG = document.createElement('div');
 sendMSG.classList.add('inputMsg');
-sendMSG.innerHTML = `<form action="mailto:gabdulrahman.mohammed@gmail.com" method="post" enctype="text/plain"><div style=color:#000;margin-bottom:10px; >Let me know your name LEGEND (*o*)</div><input onclick='(event){event.stopPropagation()
-}' type=text placeholder=Enter-your-name name=name /><button type=submit >Submit</button></form>`;
+sendMSG.innerHTML = `<form action="mailto:gabdulrahman.mohammed@gmail.com" method="post" enctype="text/plain"><div style=color:#000;margin-bottom:10px; >Let me know your name LEGEND (*o*)</div><input onclick='st(event)' type=text placeholder=Enter-your-name name=name /><button type=submit >Submit</button></form>`;
+st=(e)=>{
+event.stopPropagation()
+}
 let sTimer;
 randomPoint.onmouseover = () => {
     sTimer=setTimeout(()=>{
